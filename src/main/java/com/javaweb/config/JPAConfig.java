a33f2670ebaf4e93df5bc8868ad6c22e99789cf0
HungCongHoang <hoangconghung189@gmail.com>
2024-09-16 17:27:57 +0700

    commit security part 2 16-9

--- src/main/java/com/javaweb/config/JPAConfig.java
+++ src/main/java/com/javaweb/config/JPAConfig.java
@@ -1,4 +1,4 @@
-package com.javaweb.config;
+package com.laptrinhjavaweb.config;
 
 import java.util.Properties;
 
@@ -16,15 +16,22 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+//sử dụng config để chạy những config trong spring
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.javaweb.repository"})
+@EnableJpaRepositories(basePackages = {"com.laptrinhjavaweb.repository"})
+//Cho phép quản lý tự động transaction
 @EnableTransactionManagement
 public class JPAConfig {
 	
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		//method setDataSource dùng để thiết lập nguồn dữ liệu, cấu hình kết nối tới Database
 		em.setDataSource(dataSource());
+		//Để làm việc với JPA ta cần có 1 file persistence-data 
+		//là 1 file chứa các nhóm enity
+		//là file dùng để entity của các bảng sao
+		//và các bảng gốc trong database giao tiếp với nhau
 		em.setPersistenceUnitName("persistence-data");
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		em.setJpaVendorAdapter(vendorAdapter);
@@ -33,8 +40,10 @@ public class JPAConfig {
 	}
 	
 	@Bean
+	//quản lý các transaction
 	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
 		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		//Gắn EntityManagerFactory vào transactionManager, để quản lý các entity và thực hiện giao dịch (transaction) với cơ sở dữ liệu.
 		transactionManager.setEntityManagerFactory(entityManagerFactory);
 		return transactionManager;
 	}
@@ -47,18 +56,21 @@ public class JPAConfig {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		dataSource.setUrl("jdbc:sqlserver://DESKTOP-5OGVL8L\\SQLEXPRESS:1433;databaseName=SpringMVC_DB;encrypt=true;trustServerCertificate=true;");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("123456");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/springmvcbasicfree");
+		dataSource.setUsername("root");
+		dataSource.setPassword("1234");
 		return dataSource;
 	}
 	
 	Properties additionalProperties() {
 		Properties properties = new Properties();
 		//properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		//lệnh creat để tạo database từ java class entity khi chưa có databse
 		//properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		//sau khi có database, đã ổn định và có data trong databse thì ta comment lệnh create lại và dùng lệnh none bên dưới
 		properties.setProperty("hibernate.hbm2ddl.auto", "none");
+		//enable @ManyToMany(fetch = FetchType.LAZY)
 		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
 		return properties;
 	}
---
+++ src/main/java/com/javaweb/config/JpaAuditingConfig.java
@@ -0,0 +1,34 @@
+package com.laptrinhjavaweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
+@Configuration
+//@EnableJpaAuditing(auditorAwareRef = "auditorProvider"): Kích hoạt tính năng JPA Auditing trong Spring Data JPA. 
+//Nó tự động theo dõi và ghi lại các trường như createdBy, modifiedBy. 
+//auditorAwareRef = "auditorProvider" chỉ ra rằng Spring sẽ dùng bean auditorProvider để lấy thông tin về người thực hiện thao tác.
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfig {
+	
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return new AuditorAwareImpl();
+	}
+	
+	public static class AuditorAwareImpl implements AuditorAware<String> {
+
+		@Override
+		public String getCurrentAuditor() {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			if (authentication == null) {
+				return null;
+			}
+			return authentication.getName();
+		}
+	}
+}
